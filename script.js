cf1973002d62531aab7ccc1aabfa33162100a898
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-09-24 15:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your code here
+const inputVal = document.getElementById("evaluatedText");
+const h3Text = document.getElementById("wordCount");
+
+
+inputVal.addEventListener("input",(e)=>{
+
+    // console.log(e);
+
+    const text = inputVal.value.trim("");
+    const words = text ? text.split(/\s+/):[];
+
+    h3Text.innerText = words.length
+
+})
